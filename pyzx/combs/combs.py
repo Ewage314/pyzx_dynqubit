7b119129b74487e05064bc036e1caaebf22de3bc
Ewan <ewan.murphy360@gmail.com>
2023-02-05 21:07:07 +0000

    Starting to benchmark

--- pyzx/combs/combs.py
+++ pyzx/combs/combs.py
@@ -1,4 +1,5 @@
 import sys
+import pyzx as zx
 from pyzx.generate import cnots as generate_cnots
 from pyzx.circuit import Circuit, gate_types
 from pyzx.linalg import Mat2
@@ -213,6 +214,214 @@ class CNOTComb(CNOT_tracker):
         self.holes = holes
         self.new_to_old_qubit_mappings = new_to_old_qubit_mappings
 
+
+# UTILITY FUNCTIONS FOR COMBROWCOL
+
+
+def extract_sub_matrix(matrix, index_list):
+    new_matrix = Mat2(np.zeros([len(index_list), matrix.cols()], dtype=int))
+    for row in range(new_matrix.rows()):
+        for col in range(new_matrix.cols()):
+            new_matrix.data[row][col] = matrix.data[index_list[row]][col]
+    return new_matrix
+
+
+def insert_sub_matrix(matrix, sub_matrix, index_list):
+    for row in range(sub_matrix.rows()):
+        for col in range(sub_matrix.cols()):
+            matrix.data[index_list[row]][col] = sub_matrix.data[row][col]
+    return matrix
+
+
+def remove_rowcol(matrix, index):
+    matrix.data = np.delete(matrix.data, index, 0)
+    matrix.data = np.delete(matrix.data, index, 1)
+    print(matrix)
+
+
+def next_elimination(qubit_dependence, qubits_in_matrix, arch, rows_to_eliminate):
+    Prn = False
+
+    # Use the qubit_dependence
+    possible_eliminations = set(qubit_dependence.keys())
+    # It's easier to find all the qubits that can't be eliminated by seeing all the qubits
+    # that depend on currently unavailable qubits
+
+    # I need all the elements in possible_eliminations that aren't in qubits_in_matrix
+    unavailable_qubits = (possible_eliminations ^ set(qubits_in_matrix)) & possible_eliminations
+    # Generate a set of all the qubits that depend on currently unavailable qubits
+    impossible_eliminations = unavailable_qubits.copy()
+    for q in unavailable_qubits:
+        impossible_eliminations = impossible_eliminations.union(qubit_dependence[q])
+    # possible_eliminations now becomes all the qubits not in impossible eliminations
+    possible_eliminations = possible_eliminations ^ impossible_eliminations
+    # Find non-cutting vertices
+    non_cutting_vertices = arch.non_cutting_vertices(rows_to_eliminate)
+    non_cutting_qubits = [arch.vertex2qubit(v) for v in non_cutting_vertices]
+    #print(f"Possible Eliminations : {possible_eliminations}")
+    #print(f"Non Cutting Qubits : {non_cutting_qubits}")
+
+    eliminate = [elim for elim in possible_eliminations if (elim >= len(qubits_in_matrix) or elim in non_cutting_qubits)].pop()
+    #eliminate = possible_eliminations.pop()
+    if Prn:
+        print(f"Qubit Dependence : {qubit_dependence}")
+        print(f"Qubits in matrix : {qubits_in_matrix}")
+        print(f"Impossible Eliminations : {impossible_eliminations}")
+        print(f"Eliminate : {eliminate}")
+    return eliminate
+
+
+def combrowcol(circuit, arch, DEBUG, OUTER_DISPLAY, INNTER_DISPLAY, *args, **kwargs):
+    circ = circuit.copy()
+    OUTER_DISPLAY and display(zx.draw(circ))
+    decomposition = CombDecomposition.from_circuit(circ)
+    comb = decomposition.comb
+    OUTER_DISPLAY and display(zx.draw(comb))
+    new_comb = CNOTComb(comb.qubits, comb.holes.copy(), comb.new_to_old_qubit_mappings)
+    # Create a copy of the parity matrix of the comb to perform gaussian elimination on
+    matrix = comb.matrix.copy()
+
+    DEBUG and print(comb.matrix)
+
+    # Determine which qubits depend on the availability of other qubits
+    qubit_dependence = dict([(i, set()) for i in range(comb.qubits)])
+    for gate in comb.gates:
+        # Example, gate = CNOT(2,3)
+        # Qubit 3 now depends on qubit 2 being available
+        # All the qubits that depended on 2 being available now depend on 3 being available
+        # Qubit 2 now depends on qubit 3 being available
+        # All the qubits that depended on 3 being available now depend on 2 being available
+        qubit_dependence[gate.control].add(gate.target)
+        qubit_dependence[gate.control] = qubit_dependence[gate.control].union(qubit_dependence[gate.target])
+        qubit_dependence[gate.target].add(gate.control)
+        qubit_dependence[gate.target] = qubit_dependence[gate.target].union(qubit_dependence[gate.control])
+
+    # Iterate over the gates in the comb and log with qubits are connected via CNOTs
+    qubit_connections = dict([(i,[]) for i in range(comb.qubits)])
+    for gate in comb.gates:
+        # What we want to be doing here is not just adding connected control to target
+        # but connecting control to all the qubits connected to target
+        for q in qubit_connections[gate.target]:
+            if gate.control not in qubit_connections[q] and gate.control != q:
+                qubit_connections[q].append(gate.control)
+        for q in qubit_connections[gate.control]:
+            if gate.target not in qubit_connections[q] and gate.target != q:
+                qubit_connections[q].append(gate.target)
+
+        if gate.control not in qubit_connections[gate.target]:
+            qubit_connections[gate.target].append(gate.control)
+        if gate.target not in qubit_connections[gate.control]:
+            qubit_connections[gate.control].append(gate.target)
+        #print(f"Gate : {gate}")
+        #print(f"Qubit Connections : {qubit_connections}")
+        #qubit_connections[gate.target]  = list(set(qubit_connections[gate.target]).union(set(qubit_connections[gate.control])))
+        #qubit_connections[gate.control] = list(set(qubit_connections[gate.target]).union(set(qubit_connections[gate.control])))
+
+    #print(comb.matrix)
+    #print(f"Gates : {comb.gates}")
+
+    # Find initial qubits for the sub matrix
+    qubits_in_matrix = []
+    old_to_new_qubits = dict([(i,[]) for i in range(circ.qubits)])
+    for virtual_qubit in comb.new_to_old_qubit_mappings.keys():
+        old_to_new_qubits[comb.new_to_old_qubit_mappings[virtual_qubit]].append(virtual_qubit)
+    for logical_qubit in old_to_new_qubits.keys():
+        if len(old_to_new_qubits[logical_qubit]) == 0:
+            qubits_in_matrix.append(logical_qubit)
+        else:
+            qubits_in_matrix.append(max(old_to_new_qubits[logical_qubit]))
+
+    DEBUG and print(qubits_in_matrix)
+
+
+    # These are the qubits still accessible on the architecture
+    # This doesn't include virtual qubits
+    rows_to_eliminate = list(range(circ.qubits))
+    cols_to_eliminate = list(range(comb.qubits))
+    while 0 < len(cols_to_eliminate):
+
+        # Generate sub matrix
+        sub_matrix = extract_sub_matrix(matrix, qubits_in_matrix)
+        sub_circuit = CNOT_tracker(circ.qubits, parities_as_columns=False)
+        col_to_eliminate = next_elimination(qubit_dependence, qubits_in_matrix, arch, rows_to_eliminate)
+        DEBUG and print(comb.holes)
+        DEBUG and print(comb.new_to_old_qubit_mappings)
+
+        #print(f"Qubit Connections : {qubit_connections}")
+        #print(f"Col to eliminate : {col_to_eliminate}")
+
+        # Take account of mapping from virtual to logical qubits
+        if col_to_eliminate in comb.new_to_old_qubit_mappings:
+            row_to_eliminate = comb.new_to_old_qubit_mappings[col_to_eliminate]
+        else:
+            row_to_eliminate = col_to_eliminate
+
+        DEBUG and print(f"Qubit to eliminate {col_to_eliminate} ({row_to_eliminate})")
+
+        # Remove current rowcol from matrix
+        rowcol_iteration(sub_matrix, # need to generate correct sub matrix
+                         arch,
+                         row_to_eliminate, # rowcol needs to 'see' the virtual qubit as the original
+                         col_to_eliminate,
+                         rows_to_eliminate, # need to generate list of qubits currently accessible
+                         cols_to_eliminate,
+                         circuit=sub_circuit)
+        DEBUG and print(sub_circuit.gates)
+
+        # Convert the gates of the sub matrix using the mapping
+        for gate in sub_circuit.gates[::-1]:
+            gate.control = qubits_in_matrix[gate.control]
+            gate.target = qubits_in_matrix[gate.target]
+            # Add gates from sub circuit to comb circuit
+            new_comb.gates.insert(0, gate)
+        # Insert submatrix back into larger matrix
+        insert_sub_matrix(matrix, sub_matrix, qubits_in_matrix)
+
+        DEBUG and print(new_comb.gates)
+
+        # If the qubit just removed maps to another qubit via a hole
+        # replace that qubit with the new qubit in the qubits_in_matrix list
+        qubit_found = False
+        qubit_loc = 0
+        while not qubit_found:
+            qubit = qubits_in_matrix[qubit_loc]
+            if qubit == col_to_eliminate:
+                qubit_found = True
+                # Remove virtual qubit
+                cols_to_eliminate.remove(col_to_eliminate)
+                # Check if there are anymore virtual qubits that could be assigned to this logical one
+                if qubit in comb.holes.inverse.keys():
+                    qubits_in_matrix[qubit_loc] = comb.holes.inverse.pop(qubit)
+                else:
+                    # If no more virtual qubits can be assigned we remove it from being considered
+                    # available in the architecture
+                    if qubit in comb.new_to_old_qubit_mappings:
+                        rows_to_eliminate.remove(comb.new_to_old_qubit_mappings[qubit])
+                    else:
+                        rows_to_eliminate.remove(qubit)
+            qubit_loc += 1
+        # Remove the qubit that has just been eliminated from the connections
+        qubit_connections[col_to_eliminate] = []
+        for qubit in qubit_connections:
+            if col_to_eliminate in qubit_connections[qubit]:
+                qubit_connections[qubit].remove(col_to_eliminate)
+
+        # Remove the qubit that has just been eliminated from the connections
+        qubit_dependence.pop(col_to_eliminate)
+        for qubit in qubit_dependence:
+            if col_to_eliminate in qubit_dependence[qubit]:
+                qubit_dependence[qubit].remove(col_to_eliminate)
+
+        INNTER_DISPLAY and display(zx.draw(new_comb))
+
+    #print(f"Circuit Gates : {circ.gates}")
+    #print(f"Comb Gates : {new_comb.gates}")
+    #print(f"Holes : {decomposition.hole_plugs}")
+    decomposition.comb = new_comb
+    new_circuit = CombDecomposition.to_circuit(decomposition)
+    OUTER_DISPLAY and display(zx.draw(new_circuit))
+    return new_circuit, (matrix.data == np.eye(comb.qubits, dtype=int)).all()
+
 def rowcol_iteration(matrix, architecture, choice_row, choice_col, rows_to_eliminate, cols_to_eliminate, circuit=None, full_reduce=True, permutation=None, **kwargs):
     debug = None
     """
@@ -289,6 +498,9 @@ def rowcol_iteration(matrix, architecture, choice_row, choice_col, rows_to_elimi
     debug and print(matrix)
 
     return None
+
+# Needs to be removed, just here for comparision for the moment
+
 
 def rowcol(matrix, architecture, circuit=None, full_reduce=True, permutation=None, **kwargs):
     """
